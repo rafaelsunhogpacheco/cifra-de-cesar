f2420b98139e1e1bde541c9bc6cd080b0c473d36
dev-rafa1707 <rafa1707@gmail.com>
2023-06-14 19:29:06 -0300

    feat: remove blank lines[B

--- cifra-de-cesar.py
+++ cifra-de-cesar.py
@@ -1,21 +1,14 @@
 
 def codificarMensagem(mensagem):
     alfabeto =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    # msgCripto = []
     msgCripto = ''
     mensagem = str(mensagem)
     mensagem = mensagem.lower()
     for letra in mensagem:
-        # print(letra)
         if letra in alfabeto:
-            # print(letra.index())
             posicao = alfabeto.index(letra)
-            # print(f'posição: {letra}')
             letraNova = alfabeto[(posicao + 3)]
-            # print(f'posicao + 3: {letraNova}')
-            # msgCripto.append(letraNova)
             msgCripto = msgCripto + letraNova
-        # print(msgCripto)
     return msgCripto
 
 teste = print(codificarMensagem('bola'))
