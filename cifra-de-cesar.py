676ee242274dcff3dbb9c467af8e5d28aee8a167
dev-rafa1707 <rafa1707@gmail.com>
2023-06-14 14:11:49 -0300

    feat: add def codificarMensagem

--- cifra-de-cesar.py
+++ cifra-de-cesar.py
@@ -2,10 +2,18 @@
 
 
 def codificarMensagem(mensagem):
+    alfabeto =  ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     mensagem = str(mensagem)
-    print(mensagem)
+    mensagem = mensagem.lower()
+    for letra in mensagem:
+        # print(letra)
+        if letra in alfabeto:
+            # print(letra.index())
+            posicao = alfabeto.index(letra)
+            print(f'posição: {posicao}')
+            LetraNova = alfabeto[(posicao + 3)]
+            print(f'posicao + 3: {LetraNova}')
 
 
 
-
-teste = codificarMensagem('Hello World')
+teste = codificarMensagem('aba')
